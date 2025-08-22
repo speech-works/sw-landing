e99ce1b95a90e29d768aab9ee5e6f0f5e439bc2c
mayankav <mayankmacav@gmail.com>
2025-08-23 03:27:35 +0530

    fix: next js config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV == "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/sw-landing",
-  assetPrefix: "/sw-landing/",
   images: {
     unoptimized: true,
   },
+  ...(isProd && {
+    basePath: "/sw-landing",
+    assetPrefix: "/sw-landing/",
+  }),
 };
 
 module.exports = nextConfig;
