f948c53dfc1182859057d72cd313ca102ac32c90
mayankav <mayankmacav@gmail.com>
2025-08-23 18:25:48 +0530

    chore: next js config change

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV == "production";
 
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  ...(isProd && {
-    basePath: "/sw-landing",
-    assetPrefix: "/sw-landing/",
-  }),
 };
 
 module.exports = nextConfig;
